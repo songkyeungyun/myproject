719e931750b4541db8094edb62511f396b4a0ab5
songkyeungyun <113000022+songkyeungyun@users.noreply.github.com>
2022-11-02 23:49:44 +0900

    코드 정리중

--- Isaac/isaac.py
+++ Isaac/isaac.py
@@ -17,22 +17,8 @@ class Isaac:
         self.frame = (self.frame + 1) % 8
         self.x += self.dir_x*5
         self.y += self.dir_y*5
-        if self.x > 700:
-            self.x = 700
-        if 245 <= self.y <= 285:
-            if self.x < 70:
-                self.x = 70
-        elif self.x < 100:
-            self.x = 100
-        if 380 <= self.x <= 420:
-            if self.y < 80:
-                self.y = 80
-            elif self.y > 430:
-                self.y = 430
-        elif self.y < 100:
-            self.y = 100
-        elif self.y > 400:
-            self.y = 400
+        self.x = clamp(100, self.x, 700)
+        self.y = clamp(100, self.y, 400)
 
     def draw(self):
         if self.dir_x == 1:
