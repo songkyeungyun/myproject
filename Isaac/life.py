45746452e8f88160b6a904389eda3a3038603ecd
songkyeungyun <113000022+songkyeungyun@users.noreply.github.com>
2022-11-11 19:38:35 +0900

    캐릭터 생명 구현

---
+++ Isaac/life.py
@@ -0,0 +1,16 @@
+from pico2d import *
+import random
+import game_world
+
+
+class Life:
+    def __init__(self):
+        Life.image = load_image('Image/life3.png')
+        self.x = 600
+        self.y = 550
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        pass
