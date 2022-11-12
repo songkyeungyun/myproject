a98059b2a4cb4fdbc2de6758a5326abbfbad9460
songkyeungyun <113000022+songkyeungyun@users.noreply.github.com>
2022-11-12 19:48:27 +0900

    아이템 획득 모션 구현

---
+++ Isaac/pick_item.py
@@ -0,0 +1,47 @@
+from pico2d import *
+import game_framework
+import Stage.stage2_state as stage2_state
+
+image = None
+logo_time = 0.0
+
+def enter():
+    global image
+    image = load_image('Image/pick_item.png')
+
+# 게임 종료 - 객체를 소멸
+def exit():
+    global image
+    del image
+
+# 게임 월드 객체를 업데이트 - 게임 로직
+def update():
+    global logo_time
+    if logo_time > 2.0:
+        logo_time = 0
+        game_framework.pop_state()
+    logo_time += 0.01
+
+    # 게임 월드 렌더링
+def draw():
+    clear_canvas()
+    stage2_state.draw_world()
+    image.draw(400, 200)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+
+
+def test_self():
+    import sys
+    this_module = sys.modules['__main__']
+    pico2d.open_canvas()
+    game_framework.run(this_module)
+    pico2d.close_canvas()
+
+
+if __name__ == '__main__':
+    test_self()
+
+
