dc2a49fab49a2e4417918ba70f1c3418d3e8dfb9
songkyeungyun <113000022+songkyeungyun@users.noreply.github.com>
2022-12-04 01:00:18 +0900

    보스 공격 패턴 수정

--- Isaac/laser.py
+++ Isaac/laser.py
@@ -1,20 +1,31 @@
 from pico2d import *
 import game_world
+import time
 
 
 class Laser:
 
     def __init__(self, x, y):
         Laser.image = load_image('Image/laser.png')
-        self.x = x
+        self.x = 400
         self.y = y
+        self.time = 0
+        self.timer = 0
+        self.cur_time = 0
 
     def draw(self):
         self.image.draw(self.x, self.y)
-        # draw_rectangle(*self.get_bb())
+        draw_rectangle(*self.get_bb())
 
     def update(self):
-        pass
+        self.cur_time = time.time()
+        self.timer = self.cur_time - self.time
+        if 2 < self.timer < 4:
+            game_world.remove_object(self)
 
     def get_bb(self):
-        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+        return self.x - 400, self.y - 30, self.x + 400, self.y + 30
+
+    def handle_collision(self, other, group):
+        if group == 'laser:red_isaac':
+            pass
